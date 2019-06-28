7f3e3d1e2d09199cf289bb7fbfeefeef6c899450
monroeshindelar <Monroeshindelar@gmail.com>
2019-06-27 21:01:13 -0700

    TODO

--- goldbot.py
+++ goldbot.py
@@ -1,4 +1,13 @@
 # TODO: Proper logging
+# TODO: Rewrite Goldbot.cs TenManCommands from the ground up
+# TODO: Actual help command for individual cogs. Overwrite the build in help commands
+# TODO: Clean up input checking for discord commands. Send less messages via await ctx.channel.send
+# TODO: HTML Image generation for current matches in ongoing sl tournament
+# TODO: Make commands more intuitive and user friendly??
+# TODO: SLCog method to get current matches
+# TODO: I don't like the way I create default sl tournament names. Think of a way to handle that better
+
+
 import logging
 from discord.ext import commands
 from _global.Config import Config
