4c553b91b91874e1d7e8bc3a4a4400cfcaac601d
monroeshindelar <Monroeshindelar@gmail.com>
2020-03-27 12:17:05 -0700

    GOL-5 Fixed issue where the bot tries to load empty data sometimes

--- utilities/Misc.py
+++ utilities/Misc.py
@@ -1,55 +1,23 @@
 import os
 import pickle
-import requests
 import logging
 
 LOGGER = logging.getLogger("goldlog")
 
 
-def read_config(file_path, delim="="):
-    config_dictionary = {}
-    with open(file_path, "r") as config_file:
-        for line in config_file:
-            line = line.strip()
-            (key, val) = line.split(delim)
-            config_dictionary[key] = val
-    return config_dictionary
-
-
-def read_or_create_file_pkl(file_path):
-    contents = None
+def read_save_file(file_path):
     if os.path.exists(file_path):
+        LOGGER.info("Misc::read_save_file - Reading data from " + file_path + " into memory")
         with open(file_path, "rb") as file:
             contents = pickle.load(file)
             file.close()
     else:
-        file = open(file_path, "wb+")
+        LOGGER.info("Misc::read_save_file - No save data exists at " + file_path + ". Starting with fresh data.")
         contents = []
-        pickle.dump(object, file, pickle.HIGHEST_PROTOCOL)
-        file.close()
     return contents
 
 
-def save_to_file_pkl(content, file_path):
+def save_file(content, file_path):
     with open(file_path, 'wb+') as file:
+        LOGGER.info("Misc::save_file - Saving data to disk at " + file_path)
         pickle.dump(content, file, pickle.HIGHEST_PROTOCOL)
-
-
-def fix_corrupt_file(file_path):
-    os.remove(file_path)
-    read_or_create_file_pkl(file_path)
-
-
-def download_image_from_web(url, file_name):
-    if os.path.isfile(file_name):
-        with open(file_name, 'wb') as handle:
-            response = requests.get(url, stream=True)
-
-            if not response.ok:
-                LOGGER.warn(response)
-
-            for block in response.iter_content(1024):
-                if not block:
-                    break
-
-                handle.write(block)
